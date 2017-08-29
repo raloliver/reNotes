81b5529c4ab5fabe2f0ec01034605050afa63c8a
Ral Oliver <contato@raloliver.com>
2017-08-29 18:24:21 -0300

    login on localStorage

--- js/login/LoginUsuario.js
+++ js/login/LoginUsuario.js
@@ -1,8 +1,17 @@
-let logado = false
+let logado = JSON.parse(localStorage.getItem("logado"))
 
 //mudar o status a depender do login
 LoginUsuario_render({
-    logado: false,
-    onLogin: () => logado = true, //cb p/ mudar o status
-    onLogout: () => logado = false
+    logado: logado,
+    usuario: localStorage.getItem("nomeUsuario"),
+    onLogin: (nomeUsuario) => {
+        logado = true
+        localStorage.setItem("logado", true) //aqui passamos dois valores, um pra o status e outro para o user
+        localStorage.setItem("nomeUsuario", nomeUsuario)
+    }, //cb p/ mudar o status
+    onLogout: () => {
+        logado = false
+        localStorage.setItem("logado", false)
+        localStorage.removeItem("nomeUsuario")        
+    }
 })
