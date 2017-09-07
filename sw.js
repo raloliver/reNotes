60d2530a7269ce58b70a6a2bf2f6c5ba771a0b19
Ral Oliver <contato@raloliver.com>
2017-09-07 13:50:39 -0300

    version of SW

--- sw.js
+++ sw.js
@@ -1,19 +1,21 @@
-//update07092017.0.1.3 (para atualização dos arquivos, apenas o comentário é suficiente)
+let semVer = 13 //para atualização dos arquivos, apenas o comentário é suficiente
 
 //quais serão os arquivos que devem ser instalados?
 let mainFiles = ["/", "img/bin2.svg", "img/edit.svg", "css/estilos.css", "css/opcoesDaPagina.css", "css/opcoesDoCartao.css", "css/cabecalho.css", "css/login.css", "css/loginForm.css", "css/loginStatus.css", "css/cartao.css", "css/novoCartao.css", "css/mural.css", "js/lib/jquery.js", "js/lib/eventemitter2.js", "js/lib/KeyBoardNavigation.js", "js/tags/Tags.js", "js/cabecalho/mudaLayout.js", "js/cabecalho/busca.js", "js/filtro/Filtro.js", "js/tipos/TiposCartao.js", "js/cartao/render/Cartao_renderHelpers.js", "js/cartao/render/CartaoOpcoes_render.js", "js/cartao/render/CartaoConteudo_render.js", "js/cartao/render/Cartao_render.js", "js/cartao/Cartao.js", "js/login/LoginUsuario_render.js", "js/login/LoginUsuario.js", "js/mural/render/Mural_render.js", "js/mural/Mural.js", "js/cabecalho/novoCartao.js"]
 
 //instalar arquivos apenas no momento da instalação do sw e apenas uma única vez
 self.addEventListener("install", function () {
-    console.log("SW delete files...")
-    //como remover os arquivos que não são mais necessários?
-    caches.delete("renotes-arquivos").then(function () {
+    console.log("SW init install...")
+    caches.open("renotes-arquivos-" + semVer).then(cache => {
         console.log("SW install files...")
-        //abrir um novo cache
-        caches.open("renotes-arquivos").then(cache => {
-            //.addAll para adicionar todos os arquivos de uma lista
-            cache.addAll(mainFiles)
-        })
+        //.addAll para adicionar todos os arquivos de uma lista
+        cache.addAll(mainFiles)
+        console.log("SW delete files...")
+        //remover versões antigas de cache
+        caches.delete("renotes-arquivos-" + (semVer - 1))
+        //como remover os arquivos que não são mais necessários?
+        caches.delete("renotes-arquivos")
+        
     })
 
 })
