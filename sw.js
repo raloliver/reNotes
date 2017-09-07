3c8d7298c689981bc041e1eef4bb8db5063b7a72
Ral Oliver <contato@raloliver.com>
2017-09-07 10:36:12 -0300

    init fixed F5 crash

--- sw.js
+++ sw.js
@@ -8,7 +8,10 @@ function loadFromCacheStorage(event) {
         //matchAll para pegar todas as imagens da lista
         return cache.match(itemReq)
         //aqui encadeamos a resposta da primeira promise
-    }).then(response => {
+    }).then(responseFromCache => {
+        //fixed problem with crash website
+        //a função fetch está disponível no SW e nesse momento passo para ele o nosso pedido, caso os arquivos não estejam no cache
+        response = responseFromCache ? responseFromCache : fetch(itemReq)
         //neste caso a res da imagem é o código dela, que pode ser convertido para base64
         //como é uma função async, o .respondWith deve ficar fora da promise
         //event.respondWith(response)
